0c30b81d443521923c85a6aebb5e47a355d56106
Bytedance <mabinshan@bytedance.com>
2021-05-23 20:36:58 +0800

    Modiify conf

--- source/conf.py
+++ source/conf.py
@@ -31,9 +31,9 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser'
+    'myst_parser',
     'recommonmark',
-    'sphinx_markdown_tables'
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
