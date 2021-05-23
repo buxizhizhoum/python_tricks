3d3405e6f3d1bea2415b3a40ed3cc5cbc3a56234
Bytedance <mabinshan@bytedance.com>
2021-05-23 20:38:40 +0800

    Remove myst_parser

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
+    # 'myst_parser',
     'recommonmark',
     'sphinx_markdown_tables',
 ]
